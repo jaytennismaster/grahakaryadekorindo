c7421d1ccdf24d11d03f3398e6b772d63481a2de
Valentio <jaytennismaster@gmail.com>
2025-08-11 12:29:39 +0700

    update

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -4,7 +4,7 @@ import { Navigation, Pagination, Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "./swiperStyles.css";
+import "./swiperstyles.css";
 
 const Carousel = ({ images, location, judul }) => {
   const totalImages = images.length;
